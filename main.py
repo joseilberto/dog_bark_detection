0322f42cb493ee793e22641a8a412a5ba0bb8b0b
Ilberto <ilbertofjunior@gmail.com>
2020-05-02 18:37:01 +0200

    Moving the record file to the pipelines folder

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ from os.path import join, dirname
 
 import os
 
-from acquisition.record import record_segment
-
+from pipelines.record import record_segment
+from pipelines.load import load_audio
 
 if __name__ == "__main__":
     audio_path = join(".", "data", "sample1.wav")
     os.makedirs(dirname(audio_path), exist_ok = True)
     check = record_segment(audio_path, length = 5)
-    
+    audio, sampling = load_audio(audio_path)
+
